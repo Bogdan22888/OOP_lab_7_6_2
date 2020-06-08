98057c0619b018f8ef8090a7c5282abbef78b7ba
strelovidniy <dwordchanel@gmail.com>
2020-06-08 12:44:00 +0300

    Add project files.

---
+++ Lab07 - 2/Product.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Lab07___2
+{
+    public class Product : IComparable
+    {
+        public int productId;
+        public string name;
+        public int weight;
+        public int productionCost;
+        public int finalCost;
+
+        public Product()
+        {
+            productId = 0;
+            name = "";
+            weight = 0;
+            productionCost = 0;
+            finalCost = 0;
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            Product prod = obj as Product;
+            if (this.finalCost < prod.finalCost)
+                return -1;
+            else if (this.finalCost > prod.finalCost)
+                return 1;
+            else
+                return 0;
+        }
+
+        public class SortByWeight : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                Product prod1 = obj1 as Product;
+                Product prod2 = obj2 as Product;
+                if (prod1.weight > prod2.weight) return 1;
+                else if (prod1.weight < prod2.weight) return -1;
+                else return 0;
+            }
+        }
+
+        public class SortByProductCost : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                Product prod1 = obj1 as Product;
+                Product prod2 = obj2 as Product;
+                if (prod1.productionCost > prod2.productionCost) return 1;
+                else if (prod1.productionCost < prod2.productionCost) return -1;
+                else return 0;
+            }
+        }
+    }
+
+    public class Products : IEnumerable
+    {
+        private Product[] products;
+        private int size;
+
+        public Products()
+        {
+            products = new Product[10];
+            size = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < size; i++) yield return products[i];
+        }
+
+        public void Add(Product p)
+        {
+            if (size >= 10) return;
+            products[size] = p;
+            size++;
+        }
+
+        public void ReadData(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+
+            Product temp;
+            string[] prodInfo = new string[5];
+            while (!reader.EndOfStream)
+            {
+                temp = new Product();
+                prodInfo = reader.ReadLine().Split(' ');
+                temp.productId = Convert.ToInt32(prodInfo[0]);
+                temp.name = prodInfo[1];
+                temp.weight = Convert.ToInt32(prodInfo[2]);
+                temp.productionCost = Convert.ToInt32(prodInfo[3]);
+                Add(temp);
+            }
+            reader.Close();
+        }
+
+        public void Edit(int index)
+        {
+            try
+            {
+                Console.WriteLine("Enter new info:");
+                Console.WriteLine("Enter product id: ");
+                products[index].productId = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter product name: ");
+                products[index].name = Console.ReadLine();
+                Console.WriteLine("Enter product weight: ");
+                products[index].weight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter production cost: ");
+                products[index].productionCost = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter production final cost: ");
+                products[index].finalCost = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Wrong format!");
+            }
+        }
+
+        public void Delete(int index)
+        {
+            List<Product> pr = new List<Product>(products);
+            pr.RemoveAt(index);
+            products = pr.ToArray();
+        }
+
+        public void Output()
+        {
+            int index = 1;
+            foreach (Product product in products)
+            {
+                Console.WriteLine("Product #" + index);
+                Console.WriteLine("Product ID: " + product.productId);
+                Console.WriteLine("Product name: " + product.name);
+                Console.WriteLine("Product production cost: " + product.productionCost);
+                Console.WriteLine("Product final cost: " + product.finalCost);
+                Console.WriteLine();
+                index++;
+            }
+        }
+
+        public void Save(string filename)
+        {
+            StreamWriter writer = new StreamWriter(filename);
+            foreach (Product product in products)
+            {
+                writer.WriteLine(product.productId + " " + product.name + " " + product.weight + " " + product.productionCost + " " + product.finalCost);
+            }
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
---
+++ Lab07 - 2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lab07___2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file name: ");
+            string filename = Console.ReadLine();
+            Products prod = new Products();
+            prod.ReadData(filename);
+
+			string menu = "\nEnter:\tA - add a new item\n\tE - edit item\n\tD - delete item\n\tO - output items\n\tS - save to a text file\n\t0 - exit the program";
+			char user;
+			do
+			{
+				Console.WriteLine(menu);
+				user = Convert.ToChar(Console.ReadLine());
+
+				switch (user)
+				{
+					case 'A':
+						Product temp = new Product();
+						Console.WriteLine("Enter product id: ");
+						temp.productId = Convert.ToInt32(Console.ReadLine());
+						temp.name = Console.ReadLine();
+						temp.weight = Convert.ToInt32(Console.ReadLine());
+						temp.productionCost = Convert.ToInt32(Console.ReadLine());
+						temp.finalCost = Convert.ToInt32(Console.ReadLine());
+						prod.Add(temp);
+						break;
+					case 'E':
+						Console.WriteLine("Enter index of what item you want to edit: ");
+						int ed = Convert.ToInt32(Console.ReadLine());
+						prod.Edit(ed);
+						break;
+					case 'D':
+						Console.WriteLine("Enter index of what item you want to delete: ");
+						int del = Convert.ToInt32(Console.ReadLine());
+						prod.Delete(del);
+						break;
+					case 'O':
+						prod.Output();
+						break;
+					case 'S':
+						Console.WriteLine("Enter a file name: ");
+						string newFilename = Console.ReadLine();
+						prod.Save(newFilename);
+						break;
+					//case 'L':
+					//	Hour leastHour = Hour.LeastPassengers(ref hours);
+					//	leastHour.Output();
+					//	break;
+					//case 'W':
+					//	Hour mostWordsHour = Hour.MostWordsInComment(ref hours);
+					//	mostWordsHour.Output();
+						//break;
+					case '0':
+						break;
+					default:
+						throw new Exception("There is no such button in menu.");
+				}
+			} while (user != '0');
+		}
+    }
+}
